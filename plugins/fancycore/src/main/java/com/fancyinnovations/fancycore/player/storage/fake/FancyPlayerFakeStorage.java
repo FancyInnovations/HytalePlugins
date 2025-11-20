3b20749b61a3c12e8035fd3e070f85289b13c99c
Oliver <oschi1303@gmail.com>
2025-11-20 11:24:20 +0100

    fancycore: Add fake storage for players

---
+++ plugins/fancycore/src/main/java/com/fancyinnovations/fancycore/player/storage/fake/FancyPlayerFakeStorage.java
@@ -0,0 +1,37 @@
+package com.fancyinnovations.fancycore.player.storage.fake;
+
+import com.fancyinnovations.fancycore.api.player.FancyPlayer;
+import com.fancyinnovations.fancycore.api.player.FancyPlayerStorage;
+
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class FancyPlayerFakeStorage implements FancyPlayerStorage {
+
+    private final Map<UUID, FancyPlayer> players;
+
+    public FancyPlayerFakeStorage() {
+        players = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    public void savePlayer(FancyPlayer player) {
+        players.put(player.getUUID(), player);
+    }
+
+    @Override
+    public FancyPlayer loadPlayer(UUID uuid) {
+        return players.get(uuid);
+    }
+
+    @Override
+    public void deletePlayer(UUID uuid) {
+        players.remove(uuid);
+    }
+
+    @Override
+    public int countPlayers() {
+        return players.size();
+    }
+}
