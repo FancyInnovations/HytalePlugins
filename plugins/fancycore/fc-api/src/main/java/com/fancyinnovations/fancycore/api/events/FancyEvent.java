bd4a6391bc17f4664605623ebffaa850d8d63a69
Oliver <oschi1303@gmail.com>
2025-11-23 18:03:37 +0100

    fancycore: Allow events to be cancellable

--- plugins/fancycore/fc-api/src/main/java/com/fancyinnovations/fancycore/api/events/FancyEvent.java
+++ plugins/fancycore/fc-api/src/main/java/com/fancyinnovations/fancycore/api/events/FancyEvent.java
@@ -3,15 +3,27 @@ package com.fancyinnovations.fancycore.api.events;
 public abstract class FancyEvent {
 
     private final long firedAt;
+    private final boolean isCancellable;
     private boolean cancelled;
 
     public FancyEvent() {
         this.firedAt = System.currentTimeMillis();
+        this.isCancellable = true;
+        this.cancelled = false;
+    }
+
+    public FancyEvent(boolean isCancellable) {
+        this.firedAt = System.currentTimeMillis();
+        this.isCancellable = isCancellable;
         this.cancelled = false;
     }
 
     public void fire()  {
+        // TODO: Implement event firing logic
+    }
 
+    public boolean isCancellable() {
+        return isCancellable;
     }
 
     public boolean isCancelled() {
