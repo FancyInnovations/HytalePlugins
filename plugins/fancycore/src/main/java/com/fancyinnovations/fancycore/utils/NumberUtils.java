566bdf2117cb9dbfdcb279c34211acfecbd25f03
Pepe Coral <coritochico@gmail.com>
2025-11-25 23:18:23 +0100

    fix: :bug: made sure always using the same locale for formating

--- plugins/fancycore/src/main/java/com/fancyinnovations/fancycore/utils/NumberUtils.java
+++ plugins/fancycore/src/main/java/com/fancyinnovations/fancycore/utils/NumberUtils.java
@@ -1,14 +1,21 @@
 package com.fancyinnovations.fancycore.utils;
 
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 
 public class NumberUtils {
 
     private static final String[] NUMBER_SUFFIXES = {
-        "", "k", "m", "b", "t", "q", "Q"
+            "", "k", "m", "b", "t", "q", "Q"
     };
 
-    private static final DecimalFormat FORMAT_TWO_DECIMALS = new DecimalFormat("#.##");
+    private static final DecimalFormat FORMAT_TWO_DECIMALS;
+
+    static {
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.US);
+        FORMAT_TWO_DECIMALS = new DecimalFormat("#.##", symbols);
+    }
 
     public static String formatNumber(double value) {
         if (Double.isNaN(value) || Double.isInfinite(value)) {
