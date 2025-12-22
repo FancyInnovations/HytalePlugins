627092bba00fb7229dadc2d60c3beb7b115d9fc7
Oliver <oschi1303@gmail.com>
2025-12-22 12:02:31 +0100

    fancycore: Add CurrencyService#get

--- plugins/fancycore/fc-api/src/main/java/com/fancyinnovations/fancycore/api/economy/CurrencyService.java
+++ plugins/fancycore/fc-api/src/main/java/com/fancyinnovations/fancycore/api/economy/CurrencyService.java
@@ -1,6 +1,12 @@
 package com.fancyinnovations.fancycore.api.economy;
 
+import com.fancyinnovations.fancycore.api.FancyCore;
+
 public interface CurrencyService {
+
+    static CurrencyService get() {
+        return FancyCore.get().getCurrencyService();
+    }
 
     Currency getCurrency(String name);
 
